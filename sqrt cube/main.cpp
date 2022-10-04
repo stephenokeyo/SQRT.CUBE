2cb46e00d5aaf072c71734450099650da358d891
stephen okeyo otieno <90698968+stephenokeyo@users.noreply.github.com>
2022-10-04 10:21:59 +0300

    Add files via upload

---
+++ sqrt cube/main.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+using namespace std;
+int main()
+{
+    char firstname
+
+    cout<<"enter your fisrt name"<<;endl
+
+    cin>> fisrt name;
+
+    cout<< your fisrt name<<;
+
+    return 0;
+    }
